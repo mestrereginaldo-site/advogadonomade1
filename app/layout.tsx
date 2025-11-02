eac57129e42cc40063943b782b626e172baf697d
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-02 09:21:39 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
-      </head>
       <body className="min-h-screen flex flex-col bg-white text-black">
         <Header />
         <main className="flex-1">
